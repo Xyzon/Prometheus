4caf1306ccee112dea18812e75562bd5ab8ff377
Joey Clazing <Clazingjoey@gmail.com>
2018-09-18 20:46:21 +0200

    Website update 0.1

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joey Clazing
+ * Date: 17-9-2018
+ * Time: 09:41
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Prometheus</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="indexcss.css">
+</head>
+<body>
+<div class="jumbotron">
+    <h1 class="display-4">Prometheus</h1>
+</div>
+
+
+<!--///////////////navbar//////////////-->
+
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Prometheus</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">ARK: Survival Evolved
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="/ark/screenshots.php">Screenshots</a></li>
+                    <li><a href="/ark/video.php">Video's</a></li>
+                    <li><a href="/ark/details.php">Details</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Paladins
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Screenshots</a></li>
+                    <li><a href="#">Video's</a></li>
+                    <li><a href="#">Details</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">The Forest
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Screenshots</a></li>
+                    <li><a href="#">Video's</a></li>
+                    <li><a href="#">Details</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">World of Warcraft
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Screenshots</a></li>
+                    <li><a href="#">Video's</a></li>
+                    <li><a href="#">Details</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">GTA V Online
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Screenshots</a></li>
+                    <li><a href="#">Video's</a></li>
+                    <li><a href="#">Details</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Conan Exiles
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Screenshots</a></li>
+                    <li><a href="#">Video's</a></li>
+                    <li><a href="#">Details</a></li>
+                </ul>
+            </li>
+    </div>
+</nav>
+
+
+<!--Nav-->
+<div class="container">
+
+</div>
+
+</body>
+</html>
